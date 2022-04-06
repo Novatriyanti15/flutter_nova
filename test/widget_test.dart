3efa2583c8c523a1fde8fbe901b9e69d3e0cf8a9
Novatriyanti15 <kanjengmasrajaassidiq@gmail.com>
2022-04-06 10:47:04 +0700

    commit operator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,73 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_nova/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  void main() {
+  //tipe data dan variable
+  //var
+  var mahasiswa = "Jejen";
+  var umur = 20;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  print(mahasiswa + " Umur = " + umur.toString());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //string
+  String mahasiswaString;
+  mahasiswaString = "Aan";
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  print(mahasiswaString);
+
+  //int
+  int semester;
+  semester = 6;
+
+  print(semester);
+
+  //double
+  double ipk;
+  ipk = 3;
+
+  print(ipk);
+
+  //boolean
+  bool benar = true;
+  bool salah = false;
+  bool tidakbenar = !true;
+  bool tidaksalah = !false;
+
+  //list
+  List jurusan = [
+    "Teknik Informatika",
+    "Sistem Informasi",
+    "DKV",
+    semester.toString(),
+    ipk.toString()
+  ];
+
+  print(jurusan);
+
+  //map
+  Map<String, dynamic> kelas = {
+    "nama": "Beben",
+    "kelas": "TI 3",
+  };
+
+  print(kelas);
+  print(kelas['nama']);
+  print(kelas['kelas']);
+
+  //Operator
+  int a, b;
+  a = 9;
+  b = 7;
+  print("a = " + a.toString());
+  print("b = " + b.toString());
+
+  print(a + b);
+  print(a - b);
+  print(a / b);
+  print(a * b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+  
 }
